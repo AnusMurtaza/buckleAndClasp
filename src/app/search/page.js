53f8aebcb9b5b0dadd3ffe83dffd639b8a8cea60
AnusMurtaza <anas.aq97@gmail.com>
2024-01-16 01:21:54 +0500

    changes

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -129,7 +129,7 @@ const page = () => {
                   console.log(value)
                   return (
                     <div className="col-lg-4 col-sm-6">
-                      {/* <Link href={`/collections/${params.slug}/products/${value.slug}`}> */}
+                      <Link href={`/collections/${value.sub_category.slug}/products/${value.slug}`}>
                         <div className="product-item" key={index}>
                           <div className="pi-pic">
                             <Image src={`${imageUrl}/${value.images[0].image}`} alt="" width={244} height={298} />
@@ -163,7 +163,7 @@ const page = () => {
                            }
                           </div>
                         </div>
-                      {/* </Link> */}
+                      </Link>
                     </div>
                   )
                 })}
